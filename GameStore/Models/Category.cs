dc308ca3fb72846d64ce2a6ee6d02c2e5f525ff4
Sami Daniel <samidaniel125@gmail.com>
2023-11-05 14:39:27 -0300

    Create Category class and Product class

---
+++ GameStore/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace GameStore.Models;
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? CategoryName { get; set; }
+    public string? CategoryDescription { get; set; }
+    public List<Product>? Products { get; set; }
+}
---
+++ GameStore/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace GameStore.Models;
+public class Product
+{
+    public int ProductId{ get; set; }
+    public string? ProductName { get; set; }
+    public string? ProductShortDescription { get; set; }
+    public string? ProductDetailedDescription { get; set;}
+    public required string ProductUrlImage { get; set; }
+    public required string ProductUrlThumbnailImage { get; set; }
+    public bool IsProductPreferred { get; set; }
+    public decimal ProductPrice { get; set; }
+    public int? CategoryId { get; set; }
+    public virtual Category? Category { get; set; }
+}
+
